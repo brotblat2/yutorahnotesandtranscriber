bcfdc7cc5953ffc7b2113b373c8012ea0c06641f
brotblat2 <rotblatbz@gmail.com>
2026-08-04 11:37:47 -0400

    Refresh web cache for link resolver

--- web-mvp/service-worker.js
+++ web-mvp/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "shiur-notes-web-v2";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "shiur-notes-web-v3";
+const ASSETS = ["./", "./index.html", "./styles.css", "./link-resolver.js", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
