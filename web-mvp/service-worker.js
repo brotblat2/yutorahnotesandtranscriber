42811ca0681a0e671835370a96207e9989851c10
brotblat2 <rotblatbz@gmail.com>
2026-08-05 20:07:06 -0400

    Cache direct PDF downloader

--- web-mvp/service-worker.js
+++ web-mvp/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "shiur-notes-web-v6";
-const ASSETS = ["./", "./index.html", "./styles.css", "./extension-theme.css", "./link-resolver.js", "./app.js", "./production-api.js", "./export-tools.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE_NAME = "shiur-notes-web-v7";
+const ASSETS = ["./", "./index.html", "./styles.css", "./extension-theme.css", "./link-resolver.js", "./app.js", "./production-api.js", "./export-tools.js", "./direct-pdf.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
